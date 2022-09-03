9ed8b42ea7e8affc32704e826adf8ce5a20897e2
Zoe <zoehayes286@gmail.com>
2022-09-03 05:14:46 -0400

    Fixing accident

--- src/components/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Button from './Button';
-
-test('Button exists', () => {
-  render(<Button name='testButton' colorClass='grey-button' width={1} handleClick={() => undefined} />)
-  const testButton = screen.getByRole("button", {name: /testButton/i})
-  expect(testButton).toBeInTheDocument()
-});
